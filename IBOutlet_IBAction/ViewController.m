90d3651079bc3834de4d9835bf9560ab6bf822cb
isaias castillo <isaias@MacBook-Air-de-isaias.local>
2013-02-03 10:07:41 -0500

    Initial Commit

---
+++ IBOutlet_IBAction/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  IBOutlet_IBAction
+//
+//  Created by isaias castillo on 3/02/13.
+//  Copyright (c) 2013 isaias castillo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
